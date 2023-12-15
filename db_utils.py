522fc24ab57e972baa32ada9f4a76bfe41fb3801
Benyam <benworku@gmail.com>
2023-12-15 13:22:27 +0000

    recommiting the docstring addiion commit

--- db_utils.py
+++ db_utils.py
@@ -5,19 +5,62 @@ import pandas as pd
 import logging
 
 class RDSDatabaseConnector:
+    """
+    A class for connecting to an RDS database, extracting data, and saving it to a local file.
+
+    Attributes:
+        config_file_path (str): The path to the configuration file.
+    
+    Methods:
+        __init__(): Initializes the RDSDatabaseConnector object, loading credentials from the specified configuration file.
+        load_config(): Loads configuration settings from the specified file path.
+        initialize_engine(): Initializes and returns a database engine using the configured RDS credentials.
+        extract_data(table_name='loan_payments'): Extracts data from the specified table in the RDS database and returns it as a Pandas DataFrame.
+        save_data(df, file_path, file_format='csv'): Saves the provided DataFrame to a file in the specified format at the given file path.
+
+    Example:
+    ```python
+    connector = RDSDatabaseConnector()
+    df = connector.extract_data(table_name='loan_payments')
+    connector.save_data(df, file_path='/path/to/save/loan_payments_data.csv')
+    ```
+    """
 
     def __init__(self):
+        """
+    Initializes the RDSDatabaseConnector object.
+
+    Loads RDS credentials from the specified configuration file and updates the object's attributes.
+
+    Attributes:
+        config_file_path (str): The path to the configuration file.
+    """
         self.config_file_path = "./credentials.yaml"
         credentials = self.load_config()
         self.__dict__.update(credentials)
 
     def load_config(self):
+        """
+    Loads configuration settings from the specified file path.
+
+    Returns:
+        dict: A dictionary containing RDS credentials.
+    
+    Raises:
+        Exception: If an error occurs while loading the configuration.
+    """
         try:
             return load_config(self.config_file_path)
         except Exception as e :
             logging.error(f"Error loading configuration from {self.config_file_path}:{e}")
             raise
     def initialize_engine(self):
+        """
+    Initializes and returns a database engine using the configured RDS credentials.
+
+    Returns:
+        sqlalchemy.engine.base.Engine: The initialized database engine.
+    """
         
         DATABASE_TYPE = 'postgresql'
         DBAPI = 'psycopg2'
@@ -25,16 +68,38 @@ class RDSDatabaseConnector:
         return engine
     
     def extract_data(self,table_name = "loan_payments"):
-       
+         
+        """
+        Extracts data from the specified table in the RDS database.
+
+        Args:
+            table_name (str, optional): The name of the table to extract data from. Default is 'loan_payments'.
+
+        Returns:
+            pandas.DataFrame: A Pandas DataFrame containing the extracted data.
+
+        """
         # Initialize the engine
         engine = self.initialize_engine()
         # Extract data into a Pandas DataFrame
         df = pd.read_sql_table(table_name, engine)
         return df
 
-    # Step 7: Now create another function which saves the data to an appropriate file format to your local machine. 
-    # This should speed up loading up the data when you're performing your EDA/analysis tasks. The function should save the data in .csv format, since we're dealing with tabular data.
+    
+    
     def save_data(self,df, file_path,file_format='csv'):
+        
+        """
+    Saves the provided DataFrame to a file in the specified format at the given file path.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame to be saved.
+        file_path (str): The path where the file should be saved.
+        file_format (str, optional): The format in which the file should be saved. Default is 'csv'.
+
+    Raises:
+        Exception: If an error occurs while saving the data to the file.
+    """
         try:
             if file_format=='csv':
                 df.to_csv(file_path,index=False)
